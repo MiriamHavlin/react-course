97669cd2a6ce50196c376c8c568ae9e5d8b81c65
Miriam <mh0527118441@gmail.com>
2022-11-21 21:49:46 +0200

    2- Basic JSX syntax

--- react-course/src/components/Survey.js
+++ react-course/src/components/Survey.js
@@ -1,6 +1,11 @@
 function Survey() {
+    const name= 'Elections 2022';
     return (
-        <h1>Survey Component</h1>
+        <div>
+            <h1>Survey Component</h1>
+            <h2>Name of Survey: {name}</h2>
+            <button onClick={(e) => console.log('Thank You!!!')}>Send</button>
+        </div>
     );
 }
 
