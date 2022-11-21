07ed874d518a14a8ce5174e5a4937bee152c6b4e
Miriam <mh0527118441@gmail.com>
2022-11-21 23:13:13 +0200

    3- Condition and Props

--- react-course/src/App.js
+++ react-course/src/App.js
@@ -3,7 +3,11 @@ import Survey from './components/Survey';
 
 function App() {
   return (
-    <Survey></Survey>
+    <>
+      <Survey name="Elections 2021" lastDate={new Date(2021, 4, 25)} answers={1562} />
+      <Survey name="Elections 2022" lastDate={new Date(2026, 6, 24)} answers={3000} />
+      <Survey name="Elections 2022" />
+    </>
   );
 }
 
--- react-course/src/components/Survey.js
+++ react-course/src/components/Survey.js
@@ -1,12 +1,21 @@
-function Survey() {
-    const name= 'Elections 2022';
+function Survey(props) {
+    const { name, lastDate, answers } = props;
+
     return (
         <div>
             <h1>Survey Component</h1>
             <h2>Name of Survey: {name}</h2>
+            {lastDate >= new Date() ?
+                <span>active until {lastDate.toDateString()}</span> :
+                <span>the survey is inactive</span>}
+            <br />
+            {answers >= 3000 && <span>Popular Survey!</span>}
+
+            <br />
             <button onClick={(e) => console.log('Thank You!!!')}>Send</button>
         </div>
     );
 }
+Survey.defaultProps = { answers: 0, lastDate: new Date() }
 
 export default Survey;
